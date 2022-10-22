3b5126de4cb791e22e0379cc2f521862ca336b88
Brendan7625 <Brendan@plover.com.au>
2022-10-22 17:27:05 +1100

    did stuff
    added layers

--- js/layers.js
+++ js/layers.js
@@ -1,20 +1,21 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("a", {
+    name: "Act Aspects", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "AA", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#FF0000",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Act Aspects", // Name of prestige currency
+    baseResource: "messages", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+        if (hasUpgrade('a', 13)) mult = mult.times(upgradeEffect('a', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -22,7 +23,59 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "s", description: "S: Gain Act Aspects", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: {
+        11: {
+            title: "Get More People",
+            description: "Double the messages sent in the server",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "The Second Time's Always Faster",
+            description: "Boost message gain by Act Aspects",
+            cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+        },
+        13: {
+            title: "Act Speedup",
+            description: "Boost Act Point gain by messages",
+            cost: new Decimal(10),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x"}
+        }
+    },
     layerShown(){return true}
+})
+addLayer("acts", {
+    name: "Acts", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Ac", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#800000",
+    requires: new Decimal(20), // Can be a function that takes requirement increases into account
+    resource: "Acts", // Name of prestige currency
+    baseResource: "Act Aspects", // Name of resource prestige is based on
+    baseAmount() {return player["a"].points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "a", description: "A: Move on to the next Act", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
 })
--- js/mod.js
+++ js/mod.js
@@ -2,25 +2,25 @@ let modInfo = {
 	name: "The Purified Chaos Tree",
 	id: "purifiedchaos",
 	author: "Brendan7625",
-	pointsName: "points",
+	pointsName: "messages",
 	modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	discordName: "Server That This Is Very Loosely Based On",
+	discordLink: "https://discord.gg/DRxn5SMZvq",
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	offlineLimit: 2,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Act Aspects",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added Act Aspects.<br>
+		- Added Acts.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -41,8 +41,9 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-
 	let gain = new Decimal(1)
+	if (hasUpgrade('a', 11)) gain = gain.times(2)
+	if (hasUpgrade('a', 12)) gain = gain.times(upgradeEffect('a', 12))
 	return gain
 }
 
