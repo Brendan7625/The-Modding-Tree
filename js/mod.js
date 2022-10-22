c97abbacb813dedd79ce62be39049896ee420831
Brendan7625 <Brendan@plover.com.au>
2022-10-22 15:58:51 +1100

    Update mod.js
    test

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Purified Chaos Tree",
+	id: "purifiedchaos",
+	author: "Brendan7625",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
